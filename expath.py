74fc427df77a5ae692d5b09df33e2962c8a7bb8c
astraylinux <astraylinux@gmail.com>
2017-03-15 00:15:58 +0800

    add re.search

--- expath.py
+++ expath.py
@@ -64,7 +64,7 @@ class PathBasic(object):
 			result = rstr
 			if ret:
 				result = ret
-		elif method == "re.sub":
+		elif method == "re.search":
 			ret = re.search(argv[0], rstr)
 			if ret:
 				result = ret.group(argv[1])
